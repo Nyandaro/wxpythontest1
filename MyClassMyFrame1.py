fe171d15eeb4d759fec1a61f3524b68f7f30cef9
Nyandaro <3908442+Nyandaro@users.noreply.github.com>
2024-05-15 08:33:19 +0900

    Update MyClassMyFrame1.py

--- MyClassMyFrame1.py
+++ MyClassMyFrame1.py
@@ -2,9 +2,20 @@
 
 import wx
 import test1
+import  a1
+import  a2
 
 # Implementing MyFrame1
 class MyClassMyFrame1( test1.MyFrame1 ):
 	def __init__( self, parent ):
 		test1.MyFrame1.__init__( self, parent )
 
+		# イベント処理時、どのメソッドを呼び出すかを登録する
+
+		self.m_button1.Bind(wx.EVT_BUTTON,self.OnButtonPress)
+
+	# イベント処理を記載
+
+	def OnButtonPress(self, event):
+		zzz = a2.streamcheck()
+		self.m_textCtrl1.SetLabel(zzz)
